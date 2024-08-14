4f32f0751b5e79e5c11239bfcad726847673aacc
Arif Khan <khanarif2738@gmail.com>
2024-08-14 22:00:14 +0530

    heading

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className='background w-full h-screen flex flex-col relative overflow-x-hidden items-center'>
       <h1 className='bg-white rounded-lg w-11/12 text-center mt-[40px] 
          px-10 py-2 text-4xl font-bold '>
-        RANDOM GIFS</h1>
+        GIPHY</h1>
       <div className='flex flex-col w-full items-center gap-y-10 mt-[30px]'>
         <Random/>
         <Tag/>
