2a644134299c528591208aa6126435bd8ba64107
Arif Khan <khanarif2738@gmail.com>
2024-08-28 00:26:17 +0530

    responsive

--- src/components/Random.js
+++ src/components/Random.js
@@ -30,7 +30,7 @@ const Random = () => {
 
 
   return (
-    <div className='w-1/2 bg-green-500 rounded-lg border border-black flex flex-col items-center
+    <div className='ml-5 mr-5 md:w-1/2 bg-green-500 rounded-lg border border-black flex flex-col items-center
         gap-y-5 mt-[15px]'>
 
         <h1 className=' mt-[15px] text-2xl underline uppercase font-bold'>A Random Gif</h1>
