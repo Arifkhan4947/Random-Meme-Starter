c790575042d802afb842eff24458bbae25ea3bbe
Arif Khan <khanarif2738@gmail.com>
2024-08-29 14:07:38 +0530

    responsive div

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -13,7 +13,7 @@ const Tag = () => {
     const {gif, loading, fetchData} = useGif(tag); 
 
   return (
-    <div className='w-1/2 bg-blue-500 rounded-lg border border-black flex flex-col items-center
+    <div className='ml-5 mr-5 mb-10 md:w-1/2 bg-blue-500 rounded-lg border border-black flex flex-col items-center
         gap-y-5 mt-[15px]'>
 
         <h1 className=' mt-[15px] text-2xl underline uppercase font-bold'>Random {tag} Gif</h1>
